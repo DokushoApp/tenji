c82978fc6c7760232c1b3ce6d162e01143125413
Uday Samsani <satyasaisamsani@gmail.com>
2025-04-16 22:25:11 -0400

    Added support for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://dokusho.app',
+	base: 'tenji',
 	// Enable React to support React JSX components.
 	integrations: [react()],
 	vite:{
