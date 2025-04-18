8d9ea3d5b2fa965d254c2762b9156bfb46a945a6
Uday Samsani <satyasaisamsani@gmail.com>
2025-04-18 15:31:00 -0400

    removed react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig, passthroughImageService } from 'astro/config';
-import react from '@astrojs/react';
 import tailwindcss from "@tailwindcss/vite";
 import deno from '@deno/astro-adapter';
 
@@ -8,8 +7,6 @@ import deno from '@deno/astro-adapter';
 export default defineConfig({
 	output: 'server',
 	adapter: deno(),
-	// Enable React to support React JSX components.
-	integrations: [react()],
 	vite:{
 		plugins:[tailwindcss()],
 	},
