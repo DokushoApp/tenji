70206c2189ea1a7cf165fdf1a2976787c140850b
Uday Samsani <satyasaisamsani@gmail.com>
2025-04-18 15:27:27 -0400

    Added dino deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from "@tailwindcss/vite";
+import deno from '@deno/astro-adapter';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
+	adapter: deno(),
 	// Enable React to support React JSX components.
 	integrations: [react()],
 	vite:{
